7a45ee87b266347c36b09b63f85e5ce697529923
mazisparrow <mazisparrow@yahoo.com>
2021-12-21 04:00:15 -0500

    watchlist fix

--- src/Components/NewWatchlist/Index.js
+++ src/Components/NewWatchlist/Index.js
@@ -260,7 +260,7 @@ export default function Index() {
                     width={setWidth()}
                   />
                   <Column 
-                    field="buyPrice" 
+                    field="currentPrice" 
                     title="Price $" 
                     filterable={false} 
                     editable={false} 
